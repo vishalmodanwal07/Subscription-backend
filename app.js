9af4499e55259796d94133a6f9d8dfd62964f0d9
vishal modanwal <vk0764394@gmail.com>
2025-05-19 02:05:25 +0530

    add global error handling and all useful middlewares

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import ConnectDB from "./database/mongodb.js";
+import errMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json()); //handle json data
+app.use(express.urlencoded({extended : 'false'})); // process form data sent via form in a simple format
+app.use(cookieParser()); //read cookies from incoming request so app can store user data
 
 app.use('/api/v1/auth' , authRouter);  //middleware
 app.use('api/v1/users' , userRouter);
 app.use('/api/v1/subscriptions' , subscriptionRouter);
+
+app.use(errMiddleware);
 
 app.get('/' , (req , res)=>{
     res.send("welcome");
---
+++ middleware/error.middleware.js
@@ -0,0 +1,32 @@
+const errMiddleware = (req , res , err , next) =>{
+    try {
+        let error = {...err};
+        error.message = err.message;
+        console.log(err); 
+
+        //mongoose bad objectId
+        if(err.name == 'CastError'){
+            const message = 'Resource not found';
+            error = new Error(message);
+        }
+
+        //mongoose duplicate key
+        if(err.code == 11000){
+            const message = 'Duplicate field value entered';
+            error = new Error(message);
+            error.statusCode = 400;
+        }
+
+        //mongoose validation error
+        if(err.name == 'ValidationError'){
+            const message = Object.values(err.errors).map(val=>val.message);
+            error = new Error(message.join(', '));
+            error.statusCode = 400;
+        }
+        res.status(error.statuscode || 500).json({success:false , erroe : error.message || 'Server Error'});
+    } catch (error) {
+        next(error);
+    }
+}
+
+export default errMiddleware;
